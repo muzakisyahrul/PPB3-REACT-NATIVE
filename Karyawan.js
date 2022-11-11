3ec04622d11965de85fc934e1ad6a095c88de865
Muzaki <muzakisyahrul100@gmail.com>
2022-11-12 00:35:17 +0700

    update

--- Karyawan.js
+++ Karyawan.js
@@ -13,6 +13,16 @@ const karyawan = [
         id:'3',nama:'Khoerul Anwar', jabatan:'Ketua Sehat',
         email:'alqhoir@stimata.ac.id', telp:'085755482039',
         gaji:'1.000.000',foto:'https://placeimg.com/100/100/tech'
+    },
+    {
+        id:'4',nama:'Muzaki Karomah', jabatan:'Mahasiswa',
+        email:'muzakisyahrulkaromah@stimata.ac.id', telp:'085755482039',
+        gaji:'1.000.000',foto:'https://placeimg.com/100/100/animal'
+    },
+    {
+        id:'5',nama:'Toni Sucipto', jabatan:'Scurity',
+        email:'tonisucipto@stimata.ac.id', telp:'085755482039',
+        gaji:'1.000.000',foto:'https://placeimg.com/100/100/architecture'
     }
 ];
 
